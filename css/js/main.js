765af3d0696e7ffe0ca973521656ba7b5ad04c55
SQCodes <paulsqmagadi@gmail.com>
2024-07-12 00:23:50 +0300

    new file:   css/js/main.js

---
+++ css/js/main.js
@@ -0,0 +1,24 @@
+
+//  MOBILE NAV TOGGLE
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () { navbar.classList.toggle("active"); }
+
+navToggler.addEventListener("click", toggleNavbar);
+
+
+
+
+// active header when window scrolled to 50px
+
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  window.scrollY > 50 ? header.classList.add("active")
+    : header.classList.remove("active");
+}
+
+window.addEventListener("scroll", activeHeader);
