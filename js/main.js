ef1ca53325b4c49ff330b08aa3e3300615146a7d
SQCodes <paulsqmagadi@gmail.com>
2024-07-12 01:16:20 +0300

    modified:   css/main.css 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,18 @@ const navToggler = document.querySelector(".nav-toggle-button");
 navToggler.addEventListener('click', () => {
   navbar.classList.toggle('active');
   if (navbar.classList.contains('active')) {
-    navToggler.innerHTML = '<i class="fa-solid fa-x"></i>';
+    navToggler.innerHTML = '<i class="fa-solid fa-x fa-lg"></i>';
   } else {
-    navToggler.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    navToggler.innerHTML = '<i class="fa-solid fa-bars fa-lg"></i>';
   }
 });
+
+
+const header = document.querySelector(".header");
+
+const activeHeader = function () {
+  window.scrollY > 50 ? header.classList.add("active")
+    : header.classList.remove("active");
+}
+
+window.addEventListener("scroll", activeHeader);
