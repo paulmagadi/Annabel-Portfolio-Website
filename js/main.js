1d20cf5f4767d70261c102c5b150d99c8b4a998b
SQCodes <paulsqmagadi@gmail.com>
2024-08-04 16:34:59 +0300

    deleted:    images/1.jpg 	deleted:    images/2.jpg 	deleted:    images/3-removebg.png 	deleted:    images/3.jpg 	deleted:    images/4.jpg 	deleted:    images/5.jpg 	deleted:    images/6.jpg 	deleted:    images/6r.png 	deleted:    images/6r1.png 	deleted:    images/6r2.png 	deleted:    images/6r3.png 	deleted:    images/7.jpg 	deleted:    images/8.jpg 	deleted:    images/9.jpg 	deleted:    images/9r.png 	deleted:    images/Screenshot_1721134294.png 	deleted:    images/Screenshot_1721134298.png 	deleted:    images/Screenshot_1721134363.png 	deleted:    images/Screenshot_1721134466.png 	new file:   images/annabel1r.png 	new file:   images/annabel2r.png 	new file:   images/annabel3r.png 	deleted:    images/app1.png 	modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
+
 //  MOBILE NAV TOGGLE
 
-const navbar = document.querySelector(".mobile-nav");
-const navToggler = document.querySelector(".nav-toggle-button");
+const navbar = document.querySelector('.mobile-nav');
+const navToggler = document.querySelector('.nav-toggle-button');
+const navLinks = document.querySelectorAll('.mobile-nav a');
 
 navToggler.addEventListener('click', () => {
   navbar.classList.toggle('active');
@@ -13,6 +15,16 @@ navToggler.addEventListener('click', () => {
   }
 });
 
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+      navbar.classList.toggle('active');
+      if (navbar.classList.contains('active')) {
+        navToggler.innerHTML = '<i class="fa-solid fa-x fa-lg"></i>';
+      } else {
+        navToggler.innerHTML = '<i class="fa-solid fa-bars fa-lg"></i>';
+      }
+  });
+});
 
 const header = document.querySelector(".header");
 
